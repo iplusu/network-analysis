63ad666042dcd76f086e95f146d00d9232a51d8d
iplusu <iplusu@gmail.com>
2014-10-02 13:54:00 -0400

    Changes to be committed:     new file:   g2dot.m  Changes not staged for commit: 	modified:   graphViz.m  Untracked files: 	csv/publications_indexNetwork.csv 	g2dot.m~ 	publication2.dot 	publications.mat

---
+++ g2dot.m
@@ -0,0 +1,13 @@
+tmp = [src;dst];
+utmp = unique(tmp);
+[trow, row] = ismember(src, utmp);
+[tcol, col] = ismember(dst, utmp);
+
+%tmpA = zeros(max(length(trow), length(tcol)), max(length(trow), length(tcol)));
+tmpA = sparse(max(max(row), max(col)), max(max(row), max(col)));
+for i = 1:length(row)
+    if trow(i) == 1 & tcol(i) == 1
+        tmpA(row(i), col(i)) = 1;
+    end;
+end
+graph_to_dot(tmpA, 'filename', 'publication2.dot');
