01f54337b0571d5d1b5cbb55ea2f4b8bab6198f1
iplusu <iplusu@gmail.com>
2014-10-02 13:57:14 -0400

    modified adjacent graph

--- graphViz.m
+++ graphViz.m
@@ -1,8 +1,10 @@
-[trow, row] = ismember(utmp, unique(src));
-[tcol, col] = ismember(utmp, unique(dst));
+tmp = [src;dst];
+utmp = unique(tmp);
+[trow, row] = ismember(src, utmp);
+[tcol, col] = ismember(dst, utmp);
 
-tmpA = zeros(max(length(trow), length(tcol)), max(length(trow), length(tcol)));
-
+%tmpA = zeros(max(length(trow), length(tcol)), max(length(trow), length(tcol)));
+tmpA = sparse(max(max(row), max(col)), max(max(row), max(col)));
 for i = 1:length(row)
     if trow(i) == 1 & tcol(i) == 1
         tmpA(row(i), col(i)) = 1;
